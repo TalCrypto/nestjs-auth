51c8577fa07854f2b9da3c74c0cfde07ede2079a
talcrypto <leehunter936@gmail.com>
2024-05-02 09:41:01 +0000

    chore: opt file structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,7 @@ import { CatsModule } from "./cats/cats.module";
 import { CoreModule } from "./core/core.module";
 import { AuthModule } from "./auth/auth.module";
 import { UsersModule } from "./users/users.module";
-import configuration from "./config/configuration";
+import configuration from "./common/config/configuration";
 
 @Module({
   imports: [
